b7d192812cd16c67b1d05e5a51468a0d3992261e
Jchim1102 <95406765+Jchim1102@users.noreply.github.com>
2022-11-03 13:58:13 +0800

    Update red_campus2.js

--- red_campus2.js
+++ red_campus2.js
@@ -1,7 +1,7 @@
 
 /*
 [rewrite_local]
-^http[s]?:\/\/wecard.qq.com\/kong\/fr\/health-report\/mnp\/v1\/syncapi\/get-health-info.+$ url script-response-body https://raw.githubusercontent.com/Jchim1102/Quantumult-X/main/red_campus2.js
+^http[s]?:\/\/wecard.qq.com\/kong\/fr\/health-report\/mnp\/v1\/syncapi\/get-health-info url script-response-body https://raw.githubusercontent.com/Jchim1102/Quantumult-X/main/red_campus2.js
 [mitm] 
 hostname = *.qq.*
 */
