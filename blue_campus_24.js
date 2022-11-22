83d7744716c93c760737c44b819c3399a1286724
Jchim1102 <95406765+Jchim1102@users.noreply.github.com>
2022-11-22 11:21:44 +0800

    Update blue_campus_24.js
    change to dynamic time !

--- blue_campus_24.js
+++ blue_campus_24.js
@@ -6,9 +6,20 @@
 hostname = *.qq.*
 */
 
+function timestampToTime(timestamp) {
+    var date = new Date(timestamp); // 时间戳为10位需*1000，时间戳为13位的话不需乘1000,new Date()是13位
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+    var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+    var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+    return Y + M + D + h + m;
+}
+var time = timestampToTime(new Date());
+
 var obj = JSON.parse($response.body);
 obj.status = 1;
 obj.hsjg.type = 1;
 obj.title = "已完成健康打卡";
-obj.desc = "更新于：2022-11-22 11:09";
+obj.desc = "更新于：" + time;
 $done({body: JSON.stringify(obj)});
