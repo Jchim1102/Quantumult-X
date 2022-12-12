9823b46647a00bd6d715a036817450aafbbd95bc
Jchim1102 <95406765+Jchim1102@users.noreply.github.com>
2022-12-12 21:28:42 +0800

    Update blue_campus_24.js

--- blue_campus_24.js
+++ blue_campus_24.js
@@ -20,6 +20,7 @@ var time = timestampToTime(new Date());
 var obj = JSON.parse($response.body);
 obj.status = 1;
 obj.hsjg.type = 1;
+obj.hsjg.data = time;
 obj.title = "已完成健康打卡";
 obj.desc = "更新于：" + time;
 $done({body: JSON.stringify(obj)});
